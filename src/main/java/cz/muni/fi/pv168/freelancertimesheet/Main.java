f331e2ae0bf30e8f3cff0a624925c95e4049dc3d
Tomas Madeja <t.mm.madeja@gmail.com>
2020-11-30 00:08:22 +0100

    Add exmaple pdf builder
    TODO remove eample data

--- src/main/java/cz/muni/fi/pv168/freelancertimesheet/Main.java
+++ src/main/java/cz/muni/fi/pv168/freelancertimesheet/Main.java
@@ -1,22 +1,42 @@
 package cz.muni.fi.pv168.freelancertimesheet;
 
 import cz.muni.fi.pv168.freelancertimesheet.backend.DBConnectionUtils;
+import cz.muni.fi.pv168.freelancertimesheet.backend.PDFGenerator;
 import cz.muni.fi.pv168.freelancertimesheet.backend.orm.WorkTypeImpl;
 import cz.muni.fi.pv168.freelancertimesheet.gui.MainWindow;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws URISyntaxException {
         DBConnectionUtils.init();
-//        var factory = DBConnectionUtils.getSessionFactory();
-//        var entityManager = factory.createEntityManager();
-//        entityManager.getTransaction().begin();
-////        entityManager.persist(WorkTypeImpl.createWorkType("a", "1"));
-//        entityManager.flush();
-//        entityManager.getTransaction().commit();
-//        entityManager.clear();
-//        System.out.println("Query output:");
-//        System.out.println(entityManager.createQuery("SELECT a from WorkTypeImpl a", WorkTypeImpl.class).getResultList());
+        URL res = Main.class.getClassLoader().getResource("example.html");
+        URI tmp = res.toURI();
+        File file = Paths.get(tmp).toFile();
+        try (
+                BufferedInputStream br = new BufferedInputStream(new FileInputStream(file))
+            )
+        {
+            String contents = new String(br.readAllBytes(), StandardCharsets.UTF_8);
+            PDFGenerator.savePDF(contents, "D:\\DDownloads\\example.pdf");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         MainWindow dash = MainWindow.setup();
     }
 }
