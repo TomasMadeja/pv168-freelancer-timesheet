cc81b13c3e75fa591214bb5e5c8dbc957fcf1d0d
Tomas Madeja <t.mm.madeja@gmail.com>
2020-11-30 23:31:44 +0100

    Add functionality to open pdf files in pdf reader

--- src/main/java/cz/muni/fi/pv168/freelancertimesheet/backend/PDFStorage.java
+++ src/main/java/cz/muni/fi/pv168/freelancertimesheet/backend/PDFStorage.java
@@ -1,7 +1,11 @@
 package cz.muni.fi.pv168.freelancertimesheet.backend;
 
+import cz.muni.fi.pv168.freelancertimesheet.backend.exceptions.OpeningPDFReaderException;
 import cz.muni.fi.pv168.freelancertimesheet.backend.interfaces.Invoice;
 
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -26,5 +30,29 @@ public class PDFStorage {
 
     public String toRelativePath(Path path) {
         return storageRoot.relativize(path).toString();
+    }
+
+    public boolean openInDesktop(String path) throws FileNotFoundException, OpeningPDFReaderException {
+        Path properPath = Paths.get(storageRoot.toString(), path);;
+        return openInDesktop(properPath);
+    }
+
+    public boolean openInDesktop(Path path) throws FileNotFoundException, OpeningPDFReaderException {
+        if (!path.toFile().exists()) {
+            throw new FileNotFoundException(path.toString());
+        }
+        if (!Desktop.isDesktopSupported()) {
+            return false;
+        }
+        try {
+            File myFile = new File(path.toString());
+            Desktop.getDesktop().open(myFile);
+        } catch (IOException ex) {
+            throw new OpeningPDFReaderException(
+                    "Failed to pdf file (No default reader found or error encountered on startup): " +
+                            path.toString()
+            );
+        }
+        return true;
     }
 }
