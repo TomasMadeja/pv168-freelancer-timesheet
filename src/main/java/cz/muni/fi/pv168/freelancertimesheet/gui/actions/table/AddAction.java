bd658f46f75ce008c95b1514035ba6d9bc70772b
Tomas Madeja <t.mm.madeja@gmail.com>
2020-12-02 15:52:05 +0100

    Change invoice to popup and update formmodel

---
+++ src/main/java/cz/muni/fi/pv168/freelancertimesheet/gui/actions/table/AddAction.java
@@ -0,0 +1,40 @@
+package cz.muni.fi.pv168.freelancertimesheet.gui.actions.table;
+
+import cz.muni.fi.pv168.freelancertimesheet.gui.models.FormModel;
+import cz.muni.fi.pv168.freelancertimesheet.gui.models.TableModel;
+import cz.muni.fi.pv168.freelancertimesheet.gui.models.WorkTypeTableModel;
+import cz.muni.fi.pv168.freelancertimesheet.gui.popups.worktype.forms.WorkTypeForm;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+public class AddAction extends AbstractAction {
+    private final JTable table;
+    private final FormBuilder form;
+    private final Callback successCallback;
+
+    public interface FormBuilder {
+        void build(JTable table, Callback callback);
+    }
+
+    public interface Callback {
+        void call();
+    }
+
+    public AddAction(JTable table, FormBuilder form, Callback successCallback) {
+        super("Add");
+        this.table = table;
+        this.form = form;
+        this.successCallback = successCallback;
+        putValue(SHORT_DESCRIPTION, "Add new work type");
+        putValue(MNEMONIC_KEY, KeyEvent.VK_A);
+        putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke("ctrl N"));
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        var employeeTableModel = (TableModel) table.getModel();
+        form.build(table, successCallback);
+    }
+}
---
+++ src/main/java/cz/muni/fi/pv168/freelancertimesheet/gui/actions/table/EditAction.java
@@ -0,0 +1,36 @@
+package cz.muni.fi.pv168.freelancertimesheet.gui.actions.table;
+
+import cz.muni.fi.pv168.freelancertimesheet.backend.interfaces.WorkType;
+import cz.muni.fi.pv168.freelancertimesheet.gui.models.FormModel;
+import cz.muni.fi.pv168.freelancertimesheet.gui.models.TableModel;
+import cz.muni.fi.pv168.freelancertimesheet.gui.models.WorkTypeTableModel;
+import cz.muni.fi.pv168.freelancertimesheet.gui.popups.worktype.forms.WorkTypeForm;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+public class EditAction extends AbstractAction {
+    private final JTable table;
+    private final FormBuilder form;
+
+    public interface FormBuilder {
+        <T> FormModel build(JTable table, T row);
+    }
+
+    public EditAction(JTable table, FormBuilder form) {
+        super("Edit");
+        this.table = table;
+        this.form = form;
+        putValue(SHORT_DESCRIPTION, "Edits selected work type");
+        putValue(MNEMONIC_KEY, KeyEvent.VK_E);
+        putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke("ctrl E"));
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        var tableModel = (TableModel) table.getModel();
+        int index = table.getSelectedRow();
+        form.build(table, tableModel.getRow(index));
+    }
+}
