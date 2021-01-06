412c338ff2c45b2a9f7dbbaf771219122d5cf049
Martin Mojžíšek <martin.moj@gmail.com>
2021-01-06 18:22:19 +0100

    localized Tabs

--- src/main/java/cz/muni/fi/pv168/freelancertimesheet/gui/Tabs.java
+++ src/main/java/cz/muni/fi/pv168/freelancertimesheet/gui/Tabs.java
@@ -15,6 +15,7 @@ public class Tabs extends JTabbedPane implements GenericElement {
         super(tabsPosition, tabsBehaviour);
         this.pdfStorage = pdfStorage;
     }
+    private final I18N i18n = new I18N(getClass());
 
     @Override
     public GenericElement setupLayout() {
@@ -35,9 +36,9 @@ public class Tabs extends JTabbedPane implements GenericElement {
         // need to create individual labels for each tabs
         // it allows to set custom size
         // TODO set width based on parent pane size
-        var taskLabel = new JLabel("Task", SwingConstants.CENTER);
+        var taskLabel = new JLabel(i18n.getString("task"), SwingConstants.CENTER);
         taskLabel.setPreferredSize(new Dimension(250, 30));
-        var invoiceLabel = new JLabel("Invoice", SwingConstants.CENTER);
+        var invoiceLabel = new JLabel(i18n.getString("invoice"), SwingConstants.CENTER);
         invoiceLabel.setPreferredSize(new Dimension(250, 30));
         var workTypeLabel = new JLabel("Work Type", SwingConstants.CENTER);
         workTypeLabel.setPreferredSize(new Dimension(250, 30));
